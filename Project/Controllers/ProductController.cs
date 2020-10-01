e4a8bbad832c9868e32961d72124c9f7d9eaed65
tranbach1508 <tranbach2000@gmail.com>
2020-10-01 23:36:52 +0700

    product-detail

---
+++ Project/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBLL.Repositories;
+using WebDAL.DataModels;
+
+namespace Project.Controllers
+{
+    public class ProductController : Controller
+    {
+        Repository<Product> pro = new Repository<Product>();
+        [HttpGet]
+        public ActionResult Index(string id)
+        {
+            ViewBag.product = pro.Get(id);
+            return View("~/Views/Theme/Product.cshtml");
+        }
+    }
+}
