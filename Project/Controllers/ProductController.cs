563a2c52ef52f880f5c3a16039e653bb08a7d1ae
tranbach1508 <tranbach2000@gmail.com>
2020-10-03 00:24:14 +0700

    cart

--- Project/Controllers/ProductController.cs
+++ Project/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,12 +11,57 @@ namespace Project.Controllers
 {
     public class ProductController : Controller
     {
-        Repository<Product> pro = new Repository<Product>();
+        Repository<WebDAL.DataModels.Product> pro = new Repository<WebDAL.DataModels.Product>();
         [HttpGet]
         public ActionResult Index(string id)
         {
             ViewBag.product = pro.Get(id);
             return View("~/Views/Theme/Product.cshtml");
+        }
+
+        [HttpGet]
+        public JsonResult AddToCart(string id, string qty)
+        {
+            int quantity = Convert.ToInt32(qty);
+            if (Session["cart"] == null)
+            {
+                List<Cart> cart = new List<Cart>();
+                cart.Add(new Cart { Product = pro.Get(id), Quantity = quantity });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Cart> cart = (List<Cart>)Session["cart"];
+                int index = isExist(id);
+                if (index != -1)
+                {
+                    cart[index].Quantity+= quantity;
+                }
+                else
+                {
+                    cart.Add(new Cart { Product = pro.Get(id), Quantity = quantity });
+                }
+                Session["cart"] = cart;
+            }
+            return Json("Add to cart successfull", JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Remove(string id)
+        {
+            List<Cart> cart = (List<Cart>)Session["cart"];
+            int index = isExist(id);
+            cart.RemoveAt(index);
+            Session["cart"] = cart;
+            return Json("Remove to cart successfull", JsonRequestBehavior.AllowGet);
+        }
+
+        public int isExist(string id)
+        {
+            List<Cart> cart = (List<Cart>)Session["cart"];
+            for (int i = 0; i < cart.Count; i++)
+                if (cart[i].Product.Id.Equals(id))
+                    return i;
+            return -1;
         }
     }
 }
