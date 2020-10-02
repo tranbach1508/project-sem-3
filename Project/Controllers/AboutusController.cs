144adbefba87fdc330e374c7208087df10c415bf
unknown <tranbach2000@gmail.com>
2020-10-02 09:16:06 +0700

    fasdf

---
+++ Project/Controllers/AboutusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class AboutusController : Controller
+    {
+        // GET: Aboutus
+        public ActionResult Index()
+        {
+            return View("~/Views/Theme/Aboutus.cshtml");
+        }
+    }
+}
